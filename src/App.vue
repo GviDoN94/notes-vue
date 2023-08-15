c62aed0347ce2e812e495155e60286a92f3ae245
Vladislav <vlad_surin@mail.ru>
2023-08-15 11:19:58 +0300

    Priettier config updated

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,48 @@
 <script setup>
-import { ref } from 'vue';
+  import { ref } from 'vue';
 
-const showModal = ref(false);
-const newNote = ref('');
-const notes = ref([]);
+  const showModal = ref(false);
+  const newNote = ref('');
+  const notes = ref([]);
 
-function getRandomColor() {
-  return 'hsl(' + Math.random() * 360 + ', 100%, 75%)';
-}
+  function getRandomColor() {
+    return 'hsl(' + Math.random() * 360 + ', 100%, 75%)';
+  }
 
-const addNote = () => {
-  notes.value.push({
-    id: Math.floor(Math.random() * 1000000),
-    text: newNote.value,
-    date: new Date(),
-    backgroundColor: getRandomColor()
-  });
+  const addNote = () => {
+    notes.value.push({
+      id: Math.floor(Math.random() * 1000000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor(),
+    });
 
-  showModal.value = false;
-  newNote.value = '';
-};
+    showModal.value = false;
+    newNote.value = '';
+  };
 </script>
 
 <template>
   <main>
-    <div v-if="showModal" class="overlay">
+    <div
+      v-if="showModal"
+      class="overlay"
+    >
       <div class="modal">
-        <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <textarea
+          v-model="newNote"
+          name="note"
+          id="note"
+          cols="30"
+          rows="10"
+        ></textarea>
         <button @click="addNote">Add Note</button>
-        <button @click="showModal = false" class="close">Close</button>
+        <button
+          @click="showModal = false"
+          class="close"
+        >
+          Close
+        </button>
       </div>
     </div>
     <div class="container">
@@ -39,15 +53,15 @@ const addNote = () => {
       <div class="cards-container">
         <div class="card">
           <p class="main-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam tempora molestias.
-            Laborum, animi magni!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+            aliquam tempora molestias. Laborum, animi magni!
           </p>
           <p class="date">04/27/6853</p>
         </div>
         <div class="card">
           <p class="main-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aliquam tempora molestias.
-            Laborum, animi magni!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+            aliquam tempora molestias. Laborum, animi magni!
           </p>
           <p class="date">04/27/6853</p>
         </div>
@@ -57,98 +71,98 @@ const addNote = () => {
 </template>
 
 <style scoped>
-main {
-  height: 100vh;
-  width: 100vw;
-}
+  main {
+    height: 100vh;
+    width: 100vw;
+  }
 
-.container {
-  max-width: 1000px;
-  padding: 10px;
-  margin: 0 auto;
-}
+  .container {
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto;
+  }
 
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-h1 {
-  font-weight: bold;
-  margin-bottom: 25px;
-  font-size: 75px;
-}
+  h1 {
+    font-weight: bold;
+    margin-bottom: 25px;
+    font-size: 75px;
+  }
 
-header button {
-  border: none;
-  padding: 10px;
-  width: 50px;
-  height: 50px;
-  background-color: rgb(21, 20, 20);
-  border-radius: 100%;
-  color: white;
-  font-size: 20px;
-  cursor: pointer;
-}
+  header button {
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    background-color: rgb(21, 20, 20);
+    border-radius: 100%;
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+  }
 
-.card {
-  width: 225px;
-  height: 225px;
-  background-color: rgb(237, 182, 44);
-  padding: 10px;
-  border-radius: 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-right: 20px;
-  margin-bottom: 20px;
-}
+  .card {
+    width: 225px;
+    height: 225px;
+    background-color: rgb(237, 182, 44);
+    padding: 10px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
 
-.date {
-  font-size: 12.5px;
-  font-weight: bold;
-}
+  .date {
+    font-size: 12.5px;
+    font-weight: bold;
+  }
 
-.cards-container {
-  display: flex;
-  flex-wrap: wrap;
-}
+  .cards-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-.overlay {
-  position: absolute;
-  z-index: 10;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.77);
-}
+  .overlay {
+    position: absolute;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.77);
+  }
 
-.modal {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  padding: 30px;
-  width: 750px;
-  border-radius: 10px;
-  background-color: white;
-}
+  .modal {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    padding: 30px;
+    width: 750px;
+    border-radius: 10px;
+    background-color: white;
+  }
 
-.modal button {
-  padding: 10px 20px;
-  font-size: 20px;
-  width: 20px;
-  width: 100%;
-  background-color: blueviolet;
-  color: white;
-  cursor: pointer;
-  margin-top: 15px;
-}
+  .modal button {
+    padding: 10px 20px;
+    font-size: 20px;
+    width: 20px;
+    width: 100%;
+    background-color: blueviolet;
+    color: white;
+    cursor: pointer;
+    margin-top: 15px;
+  }
 
-.modal .close {
-  background-color: rgb(193, 15, 15);
-  margin-top: 7px;
-}
+  .modal .close {
+    background-color: rgb(193, 15, 15);
+    margin-top: 7px;
+  }
 </style>
